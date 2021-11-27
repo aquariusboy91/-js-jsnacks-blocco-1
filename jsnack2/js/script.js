0ef5101e02ad8a2c0ee927f17114fb5be64adf97
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-27 11:59:54 +0100

    add ciclo for "jsnack2"

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -5,35 +5,64 @@
 //  se può partecipare o no alla festa.
 
 
+// const lista_invitati = ["luca","chiara","gerry","paolo","lavinia", 
+// "antonio","gianriccardo","fran"];
+
+
+// CICLO WHILE 
+
+// let nome_utente = prompt("inserisci il tuo nome");
+
+// i = 0;
+
+// let entrata = false;
+
+// //ciclo fintanto che non arrivo a fine lista_invitati
+// //e l'entrata è false
+// while (i < lista_invitati.length && entrata == false) 
+// {
+    
+//     if (nome_utente == lista_invitati[i]) {
+//         entrata = true;  
+//     }
+    
+//     console.log(i); 
+//     i++;
+// }
+
+// if (entrata == true) {
+//     document.writeln("puoi entrare");
+// } else {
+//     document.writeln("non puoi entrare");
+// }
+
+// console.log (entrata);
+
+
+//CICLO FOR 
+
 const lista_invitati = ["luca","chiara","gerry","paolo","lavinia", 
 "antonio","gianriccardo","fran"];
 
-
-//ciclo while 
-
 let nome_utente = prompt("inserisci il tuo nome");
 
-i = 0;
+let entrata = false; 
 
-entrata = false;
-
-//ciclo fintanto che non arrivo a fine lista_invitati
-//e l'entrata è false
-while (i < lista_invitati.length && entrata == false) 
-{
-    
+for (let i = 0; i < lista_invitati.length; i++) {
     if (nome_utente == lista_invitati[i]) {
-        entrata = true;  
+        entrata = true; 
     }
-    
-    console.log(i); 
-    i++;
 }
+
+
+console.log(lista_invitati.length);
+
 
 if (entrata == true) {
     document.writeln("puoi entrare");
 } else {
-    document.writeln("non puoi entrare");
+    document.writeln ("non puoi");
 }
 
-console.log (entrata);
+
+console.log(entrata);
